0e99f3c27d29744bcb19872d46d89ca18638dedb
Victor Gouvêa <victorgouvea28@gmail.com>
2021-11-23 11:02:34 -0300

    add arquivo bot.py

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,23 +5,24 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
     def mostra_comandos(self):
-        pass
+        for cmd in self.__comandos:
+            print(cmd, "-", self.__comandos[cmd])
 
     @abstractmethod
-    def executa_comando(self,cmd):
+    def executa_comando(self, cmd):
         pass
 
     @abstractmethod
