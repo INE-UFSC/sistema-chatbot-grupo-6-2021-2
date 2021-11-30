88888d591f1aff104b9a15cc955058edfdd6e8a3
FKettl <fekettl@gmail.com>
2021-11-30 11:23:44 -0300

    classe comando

---
+++ Bots/Comandos.py
@@ -0,0 +1,31 @@
+class Comandos:
+
+    def __init__(self, comando: str, resposta: str):
+        self.__comando = comando
+        self.__resposta = [resposta]
+
+    @property
+    def comando(self):
+        return self.__comando
+
+    @comando.setter
+    def comando(self, comando: str):
+        self.__comando = comando
+
+    @property
+    def resposta(self):
+        return self.__resposta
+    
+    @resposta.setter
+    def resposta(self, resposta: str):
+        self.__resposta = resposta
+
+    def adicionar_resposta(self, resposta: str):
+        self.resposta.append(resposta)
+
+    def remove_resposta(self, index):
+        self.resposta.pop(index)
+
+    def mostrar_respostas(self):
+        for index, x in enumerate(self.resposta):
+            print(f'{index} - {x}')
